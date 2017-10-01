13fc67e48d93cba7288ecea017e232d2329f71cd
Liu.Hanru <isuekey@github.com>
2017-10-01 14:25:29 +0800

    for debug

--- api/model/eth.model.js
+++ api/model/eth.model.js
@@ -2,7 +2,8 @@
 
 const appUtil = require("./util.js");
 const net = require('net');
-const datadir = '/Users/nevernew/var/data/ethereum/prod';
+// const datadir = '/Users/nevernew/var/data/ethereum/prod';
+const datadir = '/Users/liuhr/data/blockdata/ethereum/prod';
 
 const DomainAddress = require("../domain/database.define").DomainAddress;
 
@@ -14,105 +15,103 @@ var web3 = new Web3(new Web3.providers.IpcProvider(`${datadir}/geth.ipc`, net));
 var eth = module.exports;
 
 
-eth.bulkCreateEthAddress = function bulkCreateEthAddress(quantity, usage){
+eth.bulkCreateEthAddress = function bulkCreateEthAddress(quantity, usage) {
     let bulk = [];
-    for ( let idx = 0; idx < quantity; idx++){
-        bulk[idx] = generateCreateAddressPromise(appUtil.guid());
-    };
-    let result = [];
-    function iteratorBulk(start, total, step){
-        if(start < total){
-            return Promise.all(bulk.slice(start, start+step < total ? start+step : total)).then((values)=>{
-                let bulkData = values.map((ele)=>{
-                    return {
-                        address: ele.address,
-                        bankType: 'ETH',
-                        status: "ok",
-                        usage: usage,
-                        password: ele.password
+    return new Promise((resolve, reject) => {
+        let client = net.connect(`${datadir}/geth.ipc`);
+        client.on("connect", () => {
+            resolve(client);
+        });
+    }).then((client) => {
+        return new Promise((resolve, reject) => {
+            let resultArray = [];
+            let password, key = usage;
+            for (var start = 0; start < quantity; ++start) {
+                password = appUtil.guid();
+                client.write(JSON.stringify({ "jsonrpc": "2.0", "method": "personal_newAccount", "params": [password], "id": (start + 1) }));
+                client.on("data", (data) => {
+                    let dj = JSON.parse(data.toString());
+                    if (dj.error) {
+                        reject(dj.error);
+                    } else {
+                        resultArray.push({
+                            address: dj.result,
+                            key,
+                            password
+                        });
+                        if (start == quantity - 1) {
+                            resolve(resultArray);
+                        }
                     };
                 });
-                return DomainAddress.bulkCreate(bulkData);
-            }).then((addressInstanceArray)=>{
-                result.push.apply(result, addressInstanceArray.map((ele)=>ele.toJSON()));
-                start += step;
-                iteratorBulk(start, total, step);
-            });
-        } else {
-            return {
-                status:"ok",
-                sqldata: result.map((ele)=> {
-                    return `insert into t_lib_eth (status, address) values ('ok', '${ele.address}');`;
-                }),
-                msg:`generate ${quantity} eth address`
             };
+        });
+    }).then((resultArray) => {
+        let bulkData = values.map((ele) => {
+            return {
+                address: ele.address,
+                bankType: 'ETH',
+                status: "ok",
+                usage: usage,
+                password: ele.password
+            };
+        });
+        return DomainAddress.bulkCreate(bulkData);
+    }).then((instanceArray) => {
+        return {
+            status: "ok",
+            sqldata: instanceArray.map((ele) => {
+                return `insert into t_lib_eth (status, address) values ('ok', '${ele.toJSON().address}');`;
+            }),
+            msg: `generate ${quantity} eth address`
         };
-    };
-    return iteratorBulk(0, quantity, 5);
+
+    });
 };
 
-function generateCreateAddressPromise(password,key){
-    return new Promise((resolve, reject)=>{
-        let client = net.connect(`${datadir}/geth.ipc`, ()=>{
+function generateCreateAddressPromise(password, key) {
+    return new Promise((resolve, reject) => {
+        let client = net.connect(`${datadir}/geth.ipc`, () => {
             console.log("connect to server geth.ipc");
-            client.write(JSON.stringify({"jsonrpc":"2.0","method":"personal_newAccount","params":[password],"id":1}));
-        });
-        let dataString = '';
-        client.on('data', (data)=>{
-            dataString += data.toString();
-            console.log(dataString);
-            client.end();
-        });
-        client.on('end',()=>{
-            let data = JSON.parse(dataString);
-            if(data.error){
-                reject(data.error);
-            }else{
-                resolve({
-                    address:data.result,
-                    key,
-                    password
-                });
-            }
-            client.destroy();
+            client.write(JSON.stringify({ "jsonrpc": "2.0", "method": "personal_newAccount", "params": [password], "id": 1 }));
         });
     });
 };
 
 var ethFilter = {};
-eth.startFilter = function startFilter(){
+eth.startFilter = function startFilter() {
     return DomainAddress.findAll({
-        where:{
-            bankType:"ETH",
-            status:"ok"
+        where: {
+            bankType: "ETH",
+            status: "ok"
         }
-    }).then((addressInstanceArray)=>{
-        let addressArray = addressInstanceArray.map((ele)=> ele.toJSON());
+    }).then((addressInstanceArray) => {
+        let addressArray = addressInstanceArray.map((ele) => ele.toJSON());
         console.log(`addressArray:${JSON.stringify(addressArray)}`);
-        let address = addressArray.map((ele)=>ele.address);
+        let address = addressArray.map((ele) => ele.address);
         return subscribeLogs(address, [null, null]);
     });
 };
 
-eth.stopFilter = function stopFilter(key){
-    return new Promise((resolve, reject)=>{
+eth.stopFilter = function stopFilter(key) {
+    return new Promise((resolve, reject) => {
         let filter = ethFilter[key];
-        if(filter!= null){
-            filter.unsubscribe((error, success)=>{
-                if(success){
+        if (filter != null) {
+            filter.unsubscribe((error, success) => {
+                if (success) {
                     console.log("Success unsubscribed");
                     resolve("ok");
                 } else {
                     reject("Error when unsubscribe");
                 }
             });
-        } else{
+        } else {
             resolve("ok");
         };
     });
 };
 
-function subscribeLogs(address, topics){
+function subscribeLogs(address, topics) {
     var option = {
         fromBlock: 4249784,
         address: address || web3.eth.accounts,
@@ -120,8 +119,8 @@ function subscribeLogs(address, topics){
     };
 
     ethFilter.logs = web3.eth.filter(option);
-    ethFilter.logs.watch(function(error, result){
-        if(!error){
+    ethFilter.logs.watch(function(error, result) {
+        if (!error) {
             console.log(`no error: ${result}`);
         } else {
             console.log(`error: ${error}`);
@@ -129,10 +128,11 @@ function subscribeLogs(address, topics){
     });
     return ethFilter.logs;
 };
-function subscribeSyncing(address, topics){
-    return new Promise((resolve, reject)=>{
-        ethFilter.syncing = web3.eth.subscribe("syncing", (error, result) =>{
-            if(!error){
+
+function subscribeSyncing(address, topics) {
+    return new Promise((resolve, reject) => {
+        ethFilter.syncing = web3.eth.subscribe("syncing", (error, result) => {
+            if (!error) {
                 console.log(`no error: ${result}`);
             } else {
                 console.log(`error: ${error}`);
